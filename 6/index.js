b432442f7143227c970e32c62941b0fe9ec92cac
Xuan <xuantong2@outlook.com>
2022-12-13 00:37:05 +1100

    6

---
+++ 6/index.js
@@ -0,0 +1,14 @@
+import fs from 'node:fs';
+
+const rawData = fs.readFileSync(new URL('./input.txt', import.meta.url).pathname).toString();
+
+const dataArray = Array.from(rawData);
+
+console.log('First start of packet (4):', dataArray.findIndex(
+    (_, index) => (new Set(dataArray.slice(index - 4, index))).size === 4)
+);
+
+console.log('First start of packet (14):', dataArray.findIndex(
+    (_, index) => (new Set(dataArray.slice(index - 14, index))).size === 14)
+);
+
